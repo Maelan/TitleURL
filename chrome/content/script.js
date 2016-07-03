2dbabd4f8f36e953a385636a55e3f69fa3cd7a09
Maëlan <maelan44@msn.com>
2016-07-03 17:34:20 +0200

    Cosmetics.

--- chrome/content/script.js
+++ chrome/content/script.js
@@ -3,6 +3,8 @@ window.addEventListener("load", function _() {
 	
 	var TitleURL = {
 		
+		/** variables **/
+		
 		/* XUL nodes */
 		node:     document.getElementById("TitleURL"),
 		current:  document.getElementById("TitleURL-current"),
@@ -16,9 +18,9 @@ window.addEventListener("load", function _() {
 		tabprev:  null,
 		
 		/* CSS rules (used to handle the user preferences) */
-		css:      null,
-		rule1:    null,
-		rule2:    null,
+		css:      null,    // the stylesheet
+		rule1:    null,    // the rule for the URL input
+		rule2:    null,    // the rule for the URL input when focused
 		
 		/* Preferences handler */
 		prefs:    Components.classes["@mozilla.org/preferences-service;1"]
@@ -38,40 +40,39 @@ window.addEventListener("load", function _() {
 			}
 		},
 		
-		
+		/** functions **/
 		
 		/* Init the TitleURL.css member (search for the stylesheet applied to
 		   the XUL document which comes from this addon). */
-		initCss:        function() {
+		initCss: function() {
 			/* Search for the stylesheet applied to the XUL document which comes
 			   from this addon. */
-			for(var i = 0;  !TitleURL.css && i < document.styleSheets.length;  i++)
-				if( document.styleSheets[i].href.toLowerCase()
-				    === "chrome://titleurl/skin/aspect.css" )
+			for (var i = 0;  !TitleURL.css && i < document.styleSheets.length;  i++)
+				if (document.styleSheets[i].href.toLowerCase() === "chrome://titleurl/skin/aspect.css")
 					TitleURL.css = document.styleSheets[i]
 			
 			/* Get or create the targeted rules. */
 			var rules = TitleURL.css.cssRules;
 			var sel1 = "#urlbar .urlbar-input";
 			var sel2 = "#urlbar[focused] .urlbar-input";
-			for(var i = 0;  i < rules.length;  i++) {
-				if(rules[i].selectorText === sel1)
+			for (var i = 0;  i < rules.length;  i++) {
+				if (rules[i].selectorText === sel1)
 					TitleURL.rule1 = rules[i];
-				if(rules[i].selectorText === sel2)
+				if (rules[i].selectorText === sel2)
 					TitleURL.rule2 = rules[i];
 			}
-			if(!TitleURL.rule1) {
+			if (!TitleURL.rule1) {
 				TitleURL.css.insertRule(sel1+"{ }", rules.length);
 				TitleURL.rule1 = TitleURL.css.cssRules[rules.length-1];
 			}
-			if(!TitleURL.rule2) {
+			if (!TitleURL.rule2) {
 				TitleURL.css.insertRule(sel2+"{ }", rules.length);
 				TitleURL.rule2 = TitleURL.css.cssRules[rules.length-1];
 			}
 		},
 		
 		/* Updates things after the user has modified settings. */
-		updatePrefs:    function() {
+		updatePrefs: function() {
 			var useMono =  TitleURL.prefs.getBoolPref("use_url_monospace");
 			var useColor = TitleURL.prefs.getBoolPref("use_url_color");
 			var color =    TitleURL.prefs.getCharPref("url_color");
@@ -82,53 +83,54 @@ window.addEventListener("load", function _() {
 		
 		/* Updates the title and its style with the status of the tab
 		   (being busy or not). */
-		statusChanged:  function(e) {
-			var tab =  e.target;
+		statusChanged: function(e) {
+			var tab = e.target;
 			var node;
-			if(tab === gBrowser.selectedTab)
-				node =  TitleURL.current;
-			else if(tab === TitleURL.tabprev)
-				node =  TitleURL.preview;
-			else    return;
+			if (tab === gBrowser.selectedTab)
+				node = TitleURL.current;
+			else if (tab === TitleURL.tabprev)
+				node = TitleURL.preview;
+			else
+				return;
 			TitleURL.set(node, tab);
 		},
 		
 		/* Updates the style of the title according to the status of the tab
 		   passed (being busy or not). */
-		setStatus:      function(node, tab) {
-			if(tab.getAttribute("busy") === "true")
+		setStatus: function(node, tab) {
+			if (tab.getAttribute("busy") === "true")
 				node.classList.add("TitleURL-progress");
 			else
 				node.classList.remove("TitleURL-progress");
 		},
 		
 		/* Updates the title to the one of the tab passed. */
-		setTitle:       function(node, tab) {
-			node.value =  tab.label;
+		setTitle: function(node, tab) {
+			node.value = tab.label;
 		},
 		
 		/* Updates the preview icon to the one of the tab passed. */
-		setIcon:        function(tab) {
-			TitleURL.icon.src =  tab.image;
+		setIcon: function(tab) {
+			TitleURL.icon.src = tab.image;
 		},
 		
 		/* Updates everything at a time: title, preview icon and status. */
-		set:            function(node, tab) {
+		set: function(node, tab) {
 			TitleURL.setStatus(node, tab);
 			TitleURL.setTitle(node, tab);
-			if(node === TitleURL.preview)
+			if (node === TitleURL.preview)
 				TitleURL.setIcon(tab);
 		},
 		
 		/* Shows the current title. */
-		showCurrent:    function() {
+		showCurrent: function() {
 			TitleURL.node.classList.remove("TitleURL-preview");
 			TitleURL.node.classList.add("TitleURL-current");
 			TitleURL.idbox.classList.remove("TitleURL-preview");
 		},
 		
 		/* Shows the preview title. */
-		showPreview:    function() {
+		showPreview: function() {
 			TitleURL.node.classList.remove("TitleURL-current");
 			TitleURL.node.classList.add("TitleURL-preview");
 			TitleURL.idbox.classList.add("TitleURL-preview");
@@ -138,34 +140,34 @@ window.addEventListener("load", function _() {
 		   possibly shows it instead of the preview title if this tab is the one
 		   hovered, or hides it in favour of the preview title if another tab is
 		   hovered). */
-		setCurrent:     function() {
+		setCurrent: function() {
 			TitleURL.set(TitleURL.current, gBrowser.selectedTab);
-			if(gBrowser.selectedTab === TitleURL.tabprev)
+			if (gBrowser.selectedTab === TitleURL.tabprev)
 				TitleURL.showCurrent();
-			else if(TitleURL.tabprev != null)
+			else if (TitleURL.tabprev != null)
 				TitleURL.showPreview();
 		},
 		
 		/* Sets the preview title to the one of the hovered tab, and shows it
 		   instead of the current title (if the tab hovered is not the currently
 		   selected one). */
-		setPreview:     function(e) {
-			var tab =  e.currentTarget;
-			TitleURL.tabprev =  tab;
+		setPreview: function(e) {
+			var tab = e.currentTarget;
+			TitleURL.tabprev = tab;
 			TitleURL.set(TitleURL.preview, tab);
-			if(tab != gBrowser.selectedTab)
+			if (tab != gBrowser.selectedTab)
 				TitleURL.showPreview();
 		},
 		
 		/* Indicates that no tab is hovered anymore. */
-		unsetPreview:   function() {
-			TitleURL.tabprev =  null;
+		unsetPreview: function() {
+			TitleURL.tabprev = null;
 			TitleURL.showCurrent();
 		},
 		
 		/* Makes the tab created “hoverable”, that is to say, we can
 		   hover it to preview its title. */
-		setHoverable:   function(tab) {
+		setHoverable: function(tab) {
 			tab.addEventListener("mouseover", TitleURL.setPreview, false);
 			tab.addEventListener("mouseout", TitleURL.unsetPreview, false);
 		}
@@ -185,7 +187,7 @@ window.addEventListener("load", function _() {
 	   shown in certain circumstances, because tabprev is not set to null. To
 	   fix that: */
 	gBrowser.tabContainer.addEventListener("TabClose", function(e) {
-		if(e.target === TitleURL.tabprev)
+		if (e.target === TitleURL.tabprev)
 			TitleURL.unsetPreview();
 	}, false);
 	/* On window load, the event “tabOpen” is thrown for each tab loaded so they
@@ -200,4 +202,4 @@ window.addEventListener("load", function _() {
 	TitleURL.prefsObserver.register();
 	TitleURL.updatePrefs();
 	
-},  false);
+}, false);
